c6b15b7b8a45bb4c36817b36f09a63387d7f29dc
hekung <m18720991611@163.com>
2018-12-09 15:51:27 +0800

    初始化项目代码结构,底部tab栏创建

---
+++ src/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(item,index) in list"
+        :key="index"
+        @longtap="delOne(index)"
+      >{{item.name}}----{{item.age}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data: function() {
+    return {
+      list: [
+        { name: "hehe", age: 20 },
+        { name: "ksks", age: 13 },
+        { name: "momo", age: 120 }
+      ]
+    };
+  },
+  methods: {
+    delOne(index) {
+      wx.showModal({
+        title: "提示",
+        content: "确认删除吗",
+        success:(res) =>{
+          if (res.confirm) {
+            this.list.splice(index,1)
+          } else if (res.cancel) {
+            console.log("用户点击取消");
+          }
+        }
+      })
+     
+    }
+  }
+};
+</script>
+
+<style>
+</style>
